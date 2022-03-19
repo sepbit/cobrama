f9fe143388cc8ba873c9a2522b5b716387b5f302
Vitor Guia <contato@vitor.guia.nom.br>
2022-03-19 19:37:23 -0300

    Python 3.9

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 \
         or later (GPLv3+)',
-        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
